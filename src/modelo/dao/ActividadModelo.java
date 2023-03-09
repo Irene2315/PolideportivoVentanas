77d76b4d3d3b658f9e87823c84a1cd4989528e56
admin <admin@DESKTOP-UJGJBG5>
2023-03-09 14:26:29 +0100

    nuebos cambios actividad

--- src/modelo/dao/ActividadModelo.java
+++ src/modelo/dao/ActividadModelo.java
@@ -7,24 +7,9 @@ import java.sql.SQLException;
 import modelo.Conector;
 
 public class ActividadModelo extends Conector {
-	public void modificarActividad(int id, String dias) {
-
-		try {
-			PreparedStatement prt = conexion.prepareStatement("UPDATE actividades SET dias_semana=? WHERE id=?");
-
-			prt.setInt(2, id);
-
-			prt.setString(1, dias);
-
-			prt.executeUpdate();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
 	
+	
+	//consigue el id de una actividad segun el nombre de la actividad introducido
 	public int getNombreActividad(String nombreActividad) {
 		int id = 0;
 		try {
@@ -42,6 +27,25 @@ public class ActividadModelo extends Conector {
 			e.printStackTrace();
 		}
 		return id;
+
+	}
+	
+	//modifica los dias de una actividad
+	public void modificarDiasActividad(int id, String dias) {
+
+		try {
+			PreparedStatement prt = conexion.prepareStatement("UPDATE actividades SET dias_semana=? WHERE id=?");
+
+			prt.setInt(2, id);
+
+			prt.setString(1, dias);
+
+			prt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
